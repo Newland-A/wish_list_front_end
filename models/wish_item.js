192bc2bf637239e3e97b8a6d1f83d6d540ae7b89
Newland-A <ajn252@gmail.com>
2020-12-07 11:37:36 -0500

    add class models

---
+++ models/wish_item.js
@@ -0,0 +1,15 @@
+class WishItem {
+  constructor(id, name, color, height, weight, link, description, price, wish_list_id){
+    this.id = id
+    this.name = name
+    this.color = color
+    this.height = height
+    this.weight = weight
+    this.link = link
+    this.description = description
+    this.price = price
+    this.wish_list_id = wish_list_id
+    // this.renderWishItem()
+  }
+}
+
---
+++ models/wish_list.js
@@ -0,0 +1,8 @@
+class WishList {
+  constructor(name, item_count, delivery_date){
+    this.name = name
+    this.item_count = item_count
+    this.delivery_date = delivery_date
+    // this.renderWishList()
+  }
+}
