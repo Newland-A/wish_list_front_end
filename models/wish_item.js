b5f0cb8145e7a356b793e1e06934e82626b1a057
Newland-A <ajn252@gmail.com>
2020-12-08 13:17:38 -0500

    create constructor for the attributes, create the html for the items cards that already exist, create method to render items to the page.

--- models/wish_item.js
+++ models/wish_item.js
@@ -9,7 +9,32 @@ class WishItem {
     this.description = description
     this.price = price
     this.wish_list_id = wish_list_id
-    // this.renderWishItem()
+    this.renderWishItems()
+  }
+
+  itemHTML() {
+    return `
+    <a href="wish_items/${this.id}"><h2 class="header">${this.name}</h2></a>
+    <img src="${this.link}" width="100" />
+    <p>Color: ${this.color}</p>
+    <p>Height: ${this.height} </p>
+    <p>Weight: ${this.weight} lbs</p>
+    <p>Link: ${this.link}</p>
+    <p>Description: ${this.description}</p>
+    <p>Price: $${this.price}.00</p>
+    <button class="add_item" id="add_item">Add Item To List</button>
+    `
+  }
+
+  renderWishItems() {
+    const itemCard = document.createElement('div')
+    itemCard.classList.add('item-card')
+    itemCard.id = this.id
+    itemCard.innerHTML += this.itemHTML()
+    itemsContainer.appendChild(itemCard)
+    itemCard.addEventListener('click', e => {
+      if (e.target.className.includes('add_item')) this.addItem(e)
+    })
   }
 }
 
