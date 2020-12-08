66eae79b7136816b48ce47e94b31cbceeb295cd3
Newland-A <ajn252@gmail.com>
2020-12-08 13:19:23 -0500

    create construtor for wish lists and define the html for the wish_lists card then render the html to the page.

--- models/wish_list.js
+++ models/wish_list.js
@@ -1,8 +1,41 @@
 class WishList {
-  constructor(name, item_count, delivery_date){
-    this.name = name
+  constructor(title, item_count = 1, delivery_date = "December 25, 2020" ){
+    this.title = title
     this.item_count = item_count
     this.delivery_date = delivery_date
-    // this.renderWishList()
+    this.renderWishLists()
   }
+
+  listHtml() {
+    return `
+    <a href="wish_lists/${this.id}"><h2 class="list-header">${this.title}</h2></a>
+    <p>Item Count: ${this.item_count}</p>
+    <p>Delivery Date: ${this.delivery_date}</p>
+    <button class="random_list" id="random_list">Use This Ready Wish List</button>
+    `
+  }
+
+  renderWishLists() {
+    const listCard = document.createElement('div')
+    listCard.classList.add('list-card')
+    listCard.id = "list-card"
+    listCard.innerHTML += this.listHtml()
+    preMadeList.appendChild(listCard)
+    listCard.addEventListener('click', e => {
+      // if (e.target.className.includes('new_list')) this.newList(e)
+    })
+  }
+
+  
+
+  // deleteWishList(e){
+  //   const id = parseInt(e.target.parentElement.id)
+  //   fetch(`http://localhost:3000/wish_lists/${id}`, {
+  //     method: 'DELETE'
+  //   })
+  //   .then(() => {
+  //     document.getElementById('your-list-card').removeChild(document.getElementById(id))
+  //   })
+  // }
+
 }
