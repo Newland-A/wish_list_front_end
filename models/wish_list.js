f73ba8fe870dbbf4c0402e8aa8252594031f27ee
Newland-A <ajn252@gmail.com>
2020-12-09 06:30:24 -0500

    adjust render function of my wish Lists

--- models/wish_list.js
+++ models/wish_list.js
@@ -21,12 +21,60 @@ class WishList {
     listCard.id = "list-card"
     listCard.innerHTML += this.listHtml()
     preMadeList.appendChild(listCard)
-    listCard.addEventListener('click', e => {
-      // if (e.target.className.includes('new_list')) this.newList(e)
-    })
+    // listCard.addEventListener('click', e => {
+    //   // if (e.target.className.includes('new_list')) this.newList(e)
+    // })
   }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
+//    newListFormHtml(id, list_title) {
+//     return `
+//     <div data-id="${id}">
+//       <p>List Title: ${list_title}</p>
+//       <input type="text" name="name" id="item_name"placeholder="Name Your Item" />
+//       <input type="text" name="color" id="item_color" placeholder="What color is it?" />
+//       <input type="text" name="height" id="item_height" placeholder="How tall is it?" />
+//       <input type="text" name="weight" id="item_weight" placeholder="How much does it weigh?" />
+//       <input type="text" name="link" id="item_link" placeholder="Link to a picture of it" />
+//       <input type="text" name="description" id="item_description" placeholder="Write a description of the Wish" />
+//       <input type="text" name="price" id="item_price" placeholder="How much does it cost?" />
+//       <button class="add_item" id="add_item">Add Item To List</button>
+//     </div>
+//     `
+//   }
+
+//  renderNestedForm(id, title) {
+  
+//   const nestedForm = document.createElement('form')
+ 
+//   nestedForm.classList.add('nested-form')
+//   nestedForm.id = this.id
+//   nestedForm.innerHTML += this.newListFormHtml(id, title)
+//   newWishList.appendChild(nestedForm)
+
+// }
 
   // deleteWishList(e){
   //   const id = parseInt(e.target.parentElement.id)
