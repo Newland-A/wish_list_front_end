4ee8810b0e06821f72ec33a04c647adccf9f7f79
Newland-A <ajn252@gmail.com>
2020-12-09 06:28:54 -0500

    Added code comments for later refactoring of more OOP oriented code

--- models/wish_item.js
+++ models/wish_item.js
@@ -32,9 +32,31 @@ class WishItem {
     itemCard.id = this.id
     itemCard.innerHTML += this.itemHTML()
     itemsContainer.appendChild(itemCard)
-    itemCard.addEventListener('click', e => {
-      if (e.target.className.includes('add_item')) this.addItem(e)
-    })
+    // itemCard.addEventListener('click', e => {
+    //   if (e.target.className.includes('add_item')) this.addItem(e)
+    // })
   }
+
+  // yourListItemHtml(list_id, list_title, item_name, item_color, item_height, item_weight, item_link, item_description, item_price) {
+  //   return `
+  //   <a href="wish_lists/${list_id}"><h2 class="list-header">${list_title}</h2></a>
+  //   <p id="yl">Name: ${item_name}</p>
+  //   <p id="yl">Color: ${item_color}</p>
+  //   <p id="yl">Height: ${item_height}</p>
+  //   <p id="yl">Weight: ${item_weight}</p>
+  //   <o id="yl">Link: ${item_link}</p>
+  //   <p id="yl">Description: ${item_description}</p>
+  //   <p id="yl">Price: ${item_price}</p>
+  //   <button class="delete">DELETE ME???</button>
+  //   `
+  // }
+  
+  // renderYourItemList() {
+  //   const yourListCard = document.createElement('div')
+  //   yourListCard.classList.add('your-list-card')
+  //   yourListCard.id = "your-list-card"
+  //   yourListCard.innerHTML += this.yourListItemHtml(list_id, list_title, item_name, item_color, item_height, item_weight, item_link, item_description, item_price)
+  //   yourLists.appendChild(yourListCard)
+  // }
 }
 
