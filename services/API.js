2373ff1f7c9392d2452a8fbdda13394e859a7e7c
Newland-A <ajn252@gmail.com>
2020-12-10 19:41:42 -0500

    add lists title to html for the items. complete rendering of list with items attached.

--- services/API.js
+++ services/API.js
@@ -16,31 +16,31 @@ class API {
 
 // posting form to the dom and grabbing the attributes inputs, clearing form after submission
 
-//   static createList(e) {
-//   //  static addNewList(e) {
-//     e.preventDefault()
-//     let list = {
-//       'title': e.target.title.value,
-//     };
-//     fetch(WISHLIST_URL, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(list)
-//     })
-//     .then(resp => resp.json())
-//     .then(wish_lists => {
-//       const { id, title} = wish_lists
-//       // debugger
-//       new WishList(id, title )
-//       renderNestedForm(id, title)
-//       document.getElementById('nested-form').addEventListener('submit', //API.addYourWishItems
-//       API.createItems
-//       )
-//       document.getElementById('nested-form').reset()
-//       })
-//   }
+  static createList(e) {
+  //  static addNewList(e) {
+    e.preventDefault()
+    let list = {
+      'title': e.target.title.value,
+    };
+    fetch(WISHLIST_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(list)
+    })
+    .then(resp => resp.json())
+    .then(wish_lists => {
+      const { id, title} = wish_lists
+      // debugger
+      new WishList(id, title )
+      renderNestedForm(id, title)
+      document.getElementById('nested-form').addEventListener('submit', //API.addYourWishItems
+      API.createItems
+      )
+      document.getElementById('nested-form').reset()
+      })
+  }
 
 // //  Wish Items
 // // fetching items
@@ -55,51 +55,14 @@ static readItem() {
       })
     })
   } 
-// // rendering the 
-//   static createItems(e) {
-//   // static addYourWishItems(e) {
-//     // debugger
-//     e.preventDefault()
-//     let list_item = {
-//       // 'title': e.target.title.value,
-//       // 'wish_item_attributes': [{
-//         'name': e.target.name.value,
-//         'color': e.target.color.value,
-//         'height': e.target.height.value,
-//         'weight': e.target.weight.value,
-//         'link': e.target.weight.value,
-//         'description': e.target.description.value,
-//         'price': e.target.price.value,
-//         'wish_list_id': e.target.dataset.id
-          
-//             // }]
-//     };
-//     // debugger
-//     fetch(WISHITEM_URL, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(list_item)
-//     })
-//     .then(resp => resp.json())
-//     .then( list_items => {
-//         // debugger
-//         const {id, name, color, height, weight, link, description, price } = list_items
-//         // debugger
-//         new WishItem(id, name, color, height, weight, link, description, price)
-        
-//         renderYourItemList(list_title, item_name, item_color, item_height, item_weight, item_link, item_description, item_price)
-        
-//         document.getElementById('nested-form').reset()
-//         })  
-//   }
-
-  static createConnectedListItem() {
+// rendering the 
+ static createItems(e) {
+  // static addYourWishItems(e) {
+    // debugger
     e.preventDefault()
     let list_item = {
-      'title': e.target.title.value,
-      'wish_item_attributes': [{
+      // 'title': e.target.title.value,
+      // 'wish_item_attributes': [{
         'name': e.target.name.value,
         'color': e.target.color.value,
         'height': e.target.height.value,
@@ -109,7 +72,7 @@ static readItem() {
         'price': e.target.price.value,
         'wish_list_id': e.target.dataset.id
           
-            }]
+            // }]
     };
     // debugger
     fetch(WISHITEM_URL, {
@@ -120,24 +83,17 @@ static readItem() {
       body: JSON.stringify(list_item)
     })
     .then(resp => resp.json())
-    .then(wish_lists => {
-      const { id, title} = wish_lists
-      // debugger
-      new WishList(id, title )
-      renderNestedForm(id, title)
-      document.getElementById('nested-form').addEventListener('submit', //API.addYourWishItems
-      API.createItems
-      )
-      document.getElementById('nested-form').reset()
-      })
     .then( list_items => {
         // debugger
-        const {id, name, color, height, weight, link, description, price } = list_items
+        const {id, name, color, height, weight, link, description, price, wish_list_id } = list_items
+        debugger
+        const title = document.getElementById('wish-title').innerText
+        new WishItem(id, name, color, height, weight, link, description, price, wish_list_id, title)
         // debugger
-        new WishItem(id, name, color, height, weight, link, description, price)
-  renderYourItemList(list_title, item_name, item_color, item_height, item_weight, item_link, item_description, item_price)
+        renderYourItemList(item_name, item_color, item_height, item_weight, item_link, item_description, item_price, wish_list_id, title)
         
-  document.getElementById('nested-form').reset()
-  })  
-  // }
+        document.getElementById('nested-form').reset()
+        })  
+  }
+
 }
