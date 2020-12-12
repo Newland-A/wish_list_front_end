ee131a7482a710b2cbedccff57acbccf6154cd76
Newland-A <ajn252@gmail.com>
2020-12-12 17:18:19 -0500

    no change

--- models/wish_list.js
+++ models/wish_list.js
@@ -4,44 +4,13 @@ class WishList {
     this.title = title
     this.item_count = item_count
     this.delivery_date = delivery_date
-    // this.renderWishLists()
-    // this.renderLists()
   }
-
-
-
-
-
-
 
  // // All Working features that can be added for later feature of things
   
-  // listHtml() {
-  //   return `
-  //   <a href="wish_lists/${this.id}"><h2 class="header">${this.title}</h2></a>
-  //   <p>Item Count: ${this.item_count}</p>
-  //   <p>Delivery Date: ${this.delivery_date}</p>
-  //   <button class="delete">DELETE THIS LIST</button>
-  //   `
-  // }
-
-  // renderLists() {
-  //   // renderWishLists() {
-  //   const listCard = document.createElement('div')
-  //   listCard.classList.add('list-card')
-  //   listCard.id = this.id
-  //   listCard.innerHTML += this.listHtml()
-  //   // preMadeList.appendChild(listCard)
-  //   listsContainer.appendChild(listCard)
-  //   // debugger
-  //   listCard.addEventListener('click', e => {
-  //     if (e.target.className.includes('delete')) this.deleteWishList(e)
-  //   })
-  // }
-
   // deleteWishList(e){
-  //   // debugger
-  //   const id = parseInt(e.target.parentElement.id)
+  //   debugger //document.getElementById
+  //   const id = parseInt(`${this.wish_list_id}`)
   //   fetch(`http://localhost:3000/wish_lists/${id}`, {
   //     method: 'DELETE'
   //   })
@@ -50,4 +19,4 @@ class WishList {
   //   })
   // }
 
-}
+}  
