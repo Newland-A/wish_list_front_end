6e8c9f4d2b28071ab228965a957d94d8589cd21d
Newland-A <ajn252@gmail.com>
2020-12-18 05:53:27 -0500

    fix unusual syntax error in code backets

--- models/wish_list.js
+++ models/wish_list.js
@@ -4,19 +4,6 @@ class WishList {
     this.title = title
     this.item_count = item_count
     this.delivery_date = delivery_date
+
   }
-
- // // All Working features that can be added for later feature of things
-  
-  // deleteWishList(e){
-  //   debugger //document.getElementById
-  //   const id = parseInt(`${this.wish_list_id}`)
-  //   fetch(`http://localhost:3000/wish_lists/${id}`, {
-  //     method: 'DELETE'
-  //   })
-  //   .then(() => {
-  //     document.getElementById('wish-list-container').removeChild(document.getElementById(id))
-  //   })
-  // }
-
-}  
+}
